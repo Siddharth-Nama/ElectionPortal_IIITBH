950d5cacb5591ac45a0bb4269256a1a6612b514d
Siddharth-Nama <siddharthnama2604@gmail.com>
2024-11-14 17:03:03 +0530

    readme.md update

--- ElectionPortal__IIITBH/account/admin.py
+++ ElectionPortal__IIITBH/account/admin.py
@@ -1,3 +1,30 @@
+# admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Define fields to display in the admin panel
+    list_display = ('email', 'first_name', 'last_name', 'user_type', 'is_staff', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('user_type', 'is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    # Specify the form layout for creating/editing users
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_type')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Fields shown when creating a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active', 'user_type'),
+        }),
+    )
+
+# Register the CustomUser model with CustomUserAdmin configuration
+admin.site.register(CustomUser, CustomUserAdmin)
--- ElectionPortal__IIITBH/voting/admin.py
+++ ElectionPortal__IIITBH/voting/admin.py
@@ -1,3 +1,29 @@
+# admin.py
 from django.contrib import admin
+from .models import Voter, Position, Candidate, Votes
 
-# Register your models here.
+class VoterAdmin(admin.ModelAdmin):
+    list_display = ('admin', 'phone', 'verified', 'voted', 'otp_sent')
+    search_fields = ('admin__email', 'admin__first_name', 'admin__last_name', 'phone')
+    list_filter = ('verified', 'voted')
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'max_vote', 'priority')
+    ordering = ('priority',)
+    search_fields = ('name',)
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('fullname', 'position')
+    search_fields = ('fullname',)
+    list_filter = ('position',)
+
+class VotesAdmin(admin.ModelAdmin):
+    list_display = ('voter', 'position', 'candidate')
+    search_fields = ('voter__admin__email', 'position__name', 'candidate__fullname')
+    list_filter = ('position',)
+
+# Register each model with its custom admin configuration
+admin.site.register(Voter, VoterAdmin)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(Votes, VotesAdmin)

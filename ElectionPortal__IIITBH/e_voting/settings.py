45a3066400ae2c387f4ee74152f38755a7253a67
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-02-15 20:57:38 +0530

    hi

--- ElectionPortal__IIITBH/e_voting/settings.py
+++ ElectionPortal__IIITBH/e_voting/settings.py
@@ -220,7 +220,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Your development static files folder
 ]
 # Directory where `collectstatic` will collect files for deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
